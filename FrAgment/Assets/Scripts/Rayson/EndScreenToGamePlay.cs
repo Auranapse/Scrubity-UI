a936213aa31afcfbe4d2835902144402a72fe702
Auranapse <AuraTigital@gmail.com>
2016-01-26 17:52:58 +0800

    - EndScreend - new icon again

---
+++ FrAgment/Assets/Scripts/Rayson/EndScreenToGamePlay.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScreenToGamePlay : MonoBehaviour {
+
+    float Timer;
+    bool Execute;
+    bool isRotateCompleted;
+    Vector3 EndRotate;
+    private RectTransform BlackBoxT, BlackBoxB;
+    public float RotationSpeed;
+    Vector3 RotateBy;
+
+	// Use this for initialization
+	void Start () {
+        isRotateCompleted = false;
+        RotateBy.Set(0, 0, 1);
+        BlackBoxT = GameObject.Find("BlackBox_Top").GetComponent<RectTransform>();
+        Execute = false;
+        BlackBoxB = GameObject.Find("BlackBox_Bottom").GetComponent<RectTransform>();
+        Timer = 0;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Execute == true)
+        {
+            Timer += Time.deltaTime;
+            if (Timer > 0)
+            {
+                if (isRotateCompleted == false)
+                {
+                    if (BlackBoxT.localEulerAngles.z < 180)
+                    {
+                        BlackBoxT.localEulerAngles += RotateBy * RotationSpeed * Time.deltaTime;
+                    }
+                    else
+                    {
+                        EndRotate.Set(0, 0, 180);
+                        BlackBoxT.localEulerAngles = EndRotate;
+
+                        isRotateCompleted = true;
+                    }
+
+                    if (BlackBoxB.localEulerAngles.z > 180)
+                    {
+                        BlackBoxB.localEulerAngles -= RotateBy * RotationSpeed * Time.deltaTime;
+                    }
+                    else
+                    {
+                        EndRotate.Set(0, 0, 180);
+                        BlackBoxB.localEulerAngles = EndRotate;
+                        isRotateCompleted = true;
+                    }
+                }
+
+                GameObject.Find("User Interface").GetComponent<Translation>().enabled = true;
+
+            }
+            if (Timer > 0.5)
+            {
+                GameObject.Find("BlackBox_Top").GetComponent<Translation>().EndPos = GameObject.Find("BlackBox_Top").GetComponent<Translation>().InitialPos;
+
+                GameObject.Find("BlackBox_Bottom").GetComponent<Translation>().EndPos = GameObject.Find("BlackBox_Bottom").GetComponent<Translation>().InitialPos;
+                
+            }
+            if (Timer > 2)
+            {
+                Application.LoadLevel("GameScene");
+            }
+        }
+    }
+
+    public void ChangeclickedState()
+    {
+        Execute = true;
+    }
+}
---
+++ FrAgment/Assets/Scripts/Rayson/Rotation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    public float StartAnimation;
+    public Vector3 RotateBy;
+    public float RotationSpeed;
+    Vector3 EndRotate;
+
+    private RectTransform BlackBoxTop,BlackBoxBot;
+
+    bool isCompleted;
+    float Timer;
+
+
+	// Use this for initialization
+	void Start () {
+        isCompleted = false;
+        Timer = 0;
+        BlackBoxTop = GameObject.Find("BlackBox_Top").GetComponent<RectTransform>();
+
+        BlackBoxBot = GameObject.Find("BlackBox_Bottom").GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isCompleted == false)
+        {
+            Timer += Time.deltaTime;
+
+            if (Timer > StartAnimation)
+            {
+                if (BlackBoxBot.localEulerAngles.z < 200)
+                {
+                    BlackBoxBot.localEulerAngles += RotateBy * RotationSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    EndRotate.Set(0, 0, 200);
+                    BlackBoxBot.localEulerAngles = EndRotate;
+                    isCompleted = true;
+                }
+
+                if (BlackBoxTop.localEulerAngles.z > 160)
+                {
+                    BlackBoxTop.localEulerAngles -= RotateBy * RotationSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    EndRotate.Set(0, 0, 160);
+                    BlackBoxTop.localEulerAngles = EndRotate;
+                    isCompleted = true;
+                }
+            }
+        }
+	}
+}
